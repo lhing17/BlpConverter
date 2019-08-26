4252bfa4c5a37eb7d83db39bacb1bdf9372845e8
lhing17 <lhing17@163.com>
2019-08-26 22:30:38 +0800

    提取方法，将输入和输出文件夹作为参数。

--- src/main/java/cn/gsein/blpconverter/image/BlpConverter.java
+++ src/main/java/cn/gsein/blpconverter/image/BlpConverter.java
@@ -20,10 +20,6 @@ import java.nio.file.Paths;
 public class BlpConverter {
 
     /**
-     * 输入文件夹
-     */
-    private static final String INPUT_DIRECTORY = "G:\\1";
-    /**
      * 主动技能图标位置
      */
     private static final String INPUT_ACTIVE = "active";
@@ -31,10 +27,6 @@ public class BlpConverter {
      * 被动技能图标位置
      */
     private static final String INPUT_PASSIVE = "passive";
-    /**
-     * 输出文件夹
-     */
-    private static final String OUTPUT_DIRECTORY = "G:\\2";
     /**
      * 替换图标目录
      */
@@ -49,11 +41,24 @@ public class BlpConverter {
     private static final String COMMAND_BUTTONS_DISABLED = "CommandButtonsDisabled";
 
     public static void main(String[] args) {
-        File activeDir = new File(Paths.get(INPUT_DIRECTORY, INPUT_ACTIVE).toString());
+        new BlpConverter().convert("G:\\1", "G:\\2");
+    }
 
+    public void convert(String inputDirectory, String outputDirectory) {
+        if (inputDirectory == null || "".equals(inputDirectory)) {
+            throw new IllegalStateException("输入文件夹为空");
+        }
+        if (outputDirectory == null || "".equals(outputDirectory)) {
+            throw new IllegalStateException("输出文件夹为空");
+        }
+        File activeDir = new File(Paths.get(inputDirectory, INPUT_ACTIVE).toString());
+
+        boolean flag = false;
         // 处理主动技能
         File[] imageFiles = activeDir.listFiles();
-        if (imageFiles != null) {
+        if (imageFiles == null || imageFiles.length == 0) {
+            flag = true;
+        } else {
             for (File imageFile : imageFiles) {
                 try {
                     // 读取源文件
@@ -67,14 +72,13 @@ public class BlpConverter {
                     BufferedImage dark = ImageUtil.addPassiveBorder(ImageUtil.adjustBrightness(resized, -64));
 
                     // 将中文名转换成拼音
-                    String originName = imageFile.getName().substring(0, imageFile.getName().indexOf("."));
-                    String name = convertToEnglishCharacter(originName);
+                    String name = getEnglishName(imageFile);
 
                     // 将文件输出为blp
                     outputAsBlp(activeBordered,
-                            Paths.get(OUTPUT_DIRECTORY, OUTPUT_REPLACEABLE, COMMAND_BUTTONS).toString(), name, "BTN");
+                            Paths.get(outputDirectory, OUTPUT_REPLACEABLE, COMMAND_BUTTONS).toString(), name, "BTN");
                     outputAsBlp(dark,
-                            Paths.get(OUTPUT_DIRECTORY, OUTPUT_REPLACEABLE, COMMAND_BUTTONS_DISABLED).toString(),
+                            Paths.get(outputDirectory, OUTPUT_REPLACEABLE, COMMAND_BUTTONS_DISABLED).toString(),
                             name, "DISBTN");
                 } catch (IOException e) {
                     e.printStackTrace();
@@ -83,10 +87,14 @@ public class BlpConverter {
         }
 
         // 处理被动技能
-        File passiveDir = new File(Paths.get(INPUT_DIRECTORY, INPUT_PASSIVE).toString());
+        File passiveDir = new File(Paths.get(inputDirectory, INPUT_PASSIVE).toString());
 
         imageFiles = passiveDir.listFiles();
-        if (imageFiles != null) {
+        if (imageFiles == null || imageFiles.length == 0) {
+            if (flag) {
+                throw new IllegalStateException("输入目录下没有合适的图片");
+            }
+        } else {
             for (File imageFile : imageFiles) {
                 try {
                     BufferedImage image = ImageIO.read(imageFile);
@@ -95,15 +103,14 @@ public class BlpConverter {
                     BufferedImage passiveBordered = ImageUtil.addPassiveBorder(resized);
                     BufferedImage dark = ImageUtil.addPassiveBorder(ImageUtil.adjustBrightness(resized, -64));
 
-                    String originName = imageFile.getName().substring(0, imageFile.getName().indexOf("."));
-                    String name = convertToEnglishCharacter(originName);
+                    String name = getEnglishName(imageFile);
 
                     //创建文件输出流
                     outputAsBlp(passiveBordered,
-                            Paths.get(OUTPUT_DIRECTORY, OUTPUT_REPLACEABLE, COMMAND_BUTTONS).toString(), name,
+                            Paths.get(outputDirectory, OUTPUT_REPLACEABLE, COMMAND_BUTTONS).toString(), name,
                             "PASBTN");
                     outputAsBlp(dark,
-                            Paths.get(OUTPUT_DIRECTORY, OUTPUT_REPLACEABLE, COMMAND_BUTTONS_DISABLED).toString(),
+                            Paths.get(outputDirectory, OUTPUT_REPLACEABLE, COMMAND_BUTTONS_DISABLED).toString(),
                             name, "DISPASBTN");
                 } catch (IOException e) {
                     e.printStackTrace();
@@ -112,17 +119,8 @@ public class BlpConverter {
         }
     }
 
-    @SuppressWarnings("ResultOfMethodCallIgnored")
-    private static void outputAsBlp(BufferedImage image, String dirPath, String name, String prefix) throws IOException {
-        File dir = new File(dirPath);
-        if (!dir.exists()) dir.mkdirs();
-        FileOutputStream out =
-                new FileOutputStream(Paths.get(dirPath, prefix + name + ".blp").toString());
-        ImageIO.write(image, "blp", out);
-        out.close();
-    }
-
-    private static String convertToEnglishCharacter(String originName) {
+    private static String getEnglishName(File imageFile) {
+        String originName = imageFile.getName().substring(0, imageFile.getName().indexOf("."));
         String name;
         try {
             HanyuPinyinOutputFormat format = new HanyuPinyinOutputFormat();
@@ -133,5 +131,17 @@ public class BlpConverter {
             name = originName;
         }
         return name;
+    }
+
+    @SuppressWarnings("ResultOfMethodCallIgnored")
+    private static void outputAsBlp(BufferedImage image, String dirPath, String name, String prefix) throws IOException {
+        File dir = new File(dirPath);
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+        FileOutputStream out =
+                new FileOutputStream(Paths.get(dirPath, prefix + name + ".blp").toString());
+        ImageIO.write(image, "blp", out);
+        out.close();
     }
 }
